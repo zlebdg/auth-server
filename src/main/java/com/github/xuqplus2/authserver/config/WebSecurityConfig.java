d72da575b62650fe2b07e568a90e882a58f998f8
xjplus <xxjplus@github.com>
2019-07-30 23:54:17 +0800

    spring security successHandler/failureHandler 对json请求返回正确信息, 而不是302重定向响应头 done

--- src/main/java/com/github/xuqplus2/authserver/config/WebSecurityConfig.java
+++ src/main/java/com/github/xuqplus2/authserver/config/WebSecurityConfig.java
@@ -3,21 +3,38 @@ package com.github.xuqplus2.authserver.config;
 import com.github.xuqplus2.authserver.service.AppDaoAuthenticationProvider;
 import com.github.xuqplus2.authserver.service.AppUserDetailsService;
 import com.github.xuqplus2.authserver.service.EncryptService;
+import com.github.xuqplus2.authserver.vo.resp.BasicResp;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.context.request.ServletWebRequest;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -40,6 +57,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     AuthenticationFailureHandler authenticationFailureHandler;
 
+    @Autowired
+    ContentNegotiationManager contentNegotiationManager;
+
+    @Autowired
+    StringHttpMessageConverter stringHttpMessageConverter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -47,9 +70,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .openidLogin().loginPage("/login.html").and() // todo to be known
                 .formLogin()
                 .loginProcessingUrl("/login")
-                .successHandler(authenticationSuccessHandler)
-                .failureHandler(authenticationFailureHandler)
                 .loginPage("/login.html") // 重写登录网页
+                .successHandler(authenticationSuccessHandler) // text/html,... | application/json(utf8)
+                .failureHandler(authenticationFailureHandler) // text/html,... | application/json(utf8)
                 .and()
                 .authorizeRequests()
                 .antMatchers("/all",
@@ -110,15 +133,50 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new AppDaoAuthenticationProvider(appUserDetailsService, delegatingPasswordEncoder);
     }
 
+    /**
+     * 期望antd前端登录成功后, 响应的header里不出现<code>302 Location 重定向</code>
+     *
+     * @return
+     */
     @Bean
     public AuthenticationSuccessHandler authenticationSuccessHandler() {
-        SimpleUrlAuthenticationSuccessHandler handler = new SimpleUrlAuthenticationSuccessHandler();
+        // 继承缺省实现, 重写关键方法
+        SavedRequestAwareAuthenticationSuccessHandler handler = new SavedRequestAwareAuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                List<MediaType> mediaTypes = contentNegotiationManager.resolveMediaTypes(new ServletWebRequest(request));
+                if (mediaTypes.contains(MediaType.APPLICATION_JSON) || mediaTypes.contains(MediaType.APPLICATION_JSON_UTF8)) {
+                    request.getRequestDispatcher("/auth/currentUser").forward(request, response);
+                } else {
+                    super.onAuthenticationSuccess(request, response, authentication);
+                }
+            }
+        };
         return handler;
     }
 
+    /**
+     * 同样, 期望antd前端登录失败时, 响应的header里不出现<code>302 Location 重定向</code>
+     *
+     * @return
+     */
     @Bean
     public AuthenticationFailureHandler authenticationFailureHandler() {
-        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler();
+        // 继承缺省实现类, 重写关键方法
+        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler() {
+
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                List<MediaType> mediaTypes = contentNegotiationManager.resolveMediaTypes(new ServletWebRequest(request));
+                if (true || mediaTypes.contains(MediaType.APPLICATION_JSON) || mediaTypes.contains(MediaType.APPLICATION_JSON_UTF8)) {
+                    log.info(exception.getMessage());
+                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                    stringHttpMessageConverter.write(new BasicResp(HttpStatus.UNAUTHORIZED.value(), exception.getMessage(), null).toJSONString(), MediaType.APPLICATION_JSON_UTF8, new ServletServerHttpResponse(response));
+                } else {
+                    super.onAuthenticationFailure(request, response, exception);
+                }
+            }
+        };
         return handler;
     }
 }
--- src/main/java/com/github/xuqplus2/authserver/controller/auth/LoginController.java
+++ src/main/java/com/github/xuqplus2/authserver/controller/auth/LoginController.java
@@ -2,18 +2,19 @@ package com.github.xuqplus2.authserver.controller.auth;
 
 import com.github.xuqplus2.authserver.vo.resp.BasicResp;
 import com.github.xuqplus2.authserver.vo.resp.CurrentUser;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("auth")
 public class LoginController {
 
-    @GetMapping("currentUser")
+    @RequestMapping(value = "currentUser", method = {RequestMethod.GET, RequestMethod.POST}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
     public ResponseEntity a() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return BasicResp.ok(new CurrentUser(authentication).toJSONString());
