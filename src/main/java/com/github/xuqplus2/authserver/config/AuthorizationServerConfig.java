e6b21e3dbb29cd4177bf3135d7e2ad659c105d0d
xjplus <xxjplus@github.com>
2019-08-06 10:10:09 +0800

    save dev

--- src/main/java/com/github/xuqplus2/authserver/config/AuthorizationServerConfig.java
+++ src/main/java/com/github/xuqplus2/authserver/config/AuthorizationServerConfig.java
@@ -10,10 +10,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
-import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * http://localhost:20010/oauth/authorize?client_id=client&response_type=code&redirect_uri=http://localhost:20008/
@@ -25,8 +26,19 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 // todo, auth server 重启导致已发出的token全部失效..
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+
+    @Autowired
+    InMemoryTokenStore inMemoryTokenStore;
+    @Autowired
+    TokenApprovalStore tokenApprovalStore;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    DataSource dataSource;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        /*
         // 使用内存存储OAuth客户端信息
         String secret = "secret";
         String noop = String.format("{noop}%s", secret);
@@ -34,9 +46,27 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         //    String scrypt = String.format("{scrypt}%s", new SCryptPasswordEncoder().encode(secret));
         clients
                 .inMemory()
-                /* client */
+                *//* client *//*
                 .withClient("client") //
                 .secret(noop) //
+                .authorizedGrantTypes(
+                        "password", "authorization_code", "implicit", "refresh_token")
+                .resourceIds("resourceId") //
+                .redirectUris(
+                        "http://aaa.local:20000/login",
+                        "http://bbb.local:20000/login",
+                        "http://dev.local:20000/login",
+                        "http://106.12.80.76:8080/login",
+                        "http://106.12.80.76:8081/login",
+                        "http://dev.local:20001/login"
+                ) //
+                .scopes("aaa", "bbb", "ccc", "ddd");*/
+        clients
+                .withClientDetails()
+                .jdbc(dataSource)
+                /* client */
+                .withClient("client") //
+                .secret("{noop}secret") //
                 .authorizedGrantTypes(
                         "password", "authorization_code", "implicit", "refresh_token")
                 .resourceIds("resourceId") //
@@ -54,8 +84,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
         endpoints
-                .tokenStore(tokenStore)
-                .approvalStore(approvalStore)
+                .tokenStore(inMemoryTokenStore)
+                .approvalStore(tokenApprovalStore)
                 .authenticationManager(authenticationManager)
                 .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
     }
@@ -69,23 +99,20 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     }
 
     @Bean
-    public TokenStore tokenStore() {
+    public InMemoryTokenStore inMemoryTokenStore() {
         return new InMemoryTokenStore();
     }
 
     @Bean
-    public ApprovalStore approvalStore() {
+    public TokenApprovalStore approvalStore() {
         TokenApprovalStore store = new TokenApprovalStore();
-        store.setTokenStore(tokenStore);
+        store.setTokenStore(inMemoryTokenStore);
         return store;
     }
 
-    @Autowired
-    TokenStore tokenStore;
+    @Bean
+    public JdbcClientDetailsService jdbcClientDetailsService() {
+        JdbcClientDetailsService service = new JdbcClientDetailsService(dataSource);
 
-    @Autowired
-    ApprovalStore approvalStore;
-
-    @Autowired
-    AuthenticationManager authenticationManager;
+    }
 }
